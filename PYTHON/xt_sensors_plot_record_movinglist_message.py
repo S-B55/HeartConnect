2850da5ef050a2f7fd201ff7af3cd421eff7eb12
Charlie Shao <charlie.shao@xethru.com>
2018-12-04 12:45:01 +0100

    change to default noisemap with around 20s initialization time

--- PYTHON/xt_sensors_plot_record_movinglist_message.py
+++ PYTHON/xt_sensors_plot_record_movinglist_message.py
@@ -55,8 +55,8 @@ x4m200_par_settings = {'detection_zone': (0.4, 2),
                        'sensitivity': 5,
                        'tx_center_frequency': 3,
                        'led_control': (XTID_LED_MODE_FULL, 0),
-                       # initialize noisemap everytime when get start (approximately 120s)
-                       'noisemap_control': 0b111,
+                       # initialize noisemap everytime when get start (approximately 20s)
+                       'noisemap_control': 0b110,
                        # only uncomment the message when you need them to avoide confliction.
                        # disable all message at first
                        #    'output_control1': (XTS_ID_BASEBAND_IQ, 0),
@@ -74,8 +74,8 @@ x4m200_par_settings = {'detection_zone': (0.4, 2),
 x4m300_par_settings = {'detection_zone': (0.4, 2),
                        'sensitivity': 5,
                        'led_control': (XTID_LED_MODE_FULL, 0),
-                       # initialize noisemap everytime when get start (approximately 120s)
-                       'noisemap_control': 0b111,
+                       # initialize noisemap everytime when get start (approximately 20s)
+                       'noisemap_control': 0b110,
                        # disable all message at first
                        #    'output_control1': (XTS_ID_BASEBAND_IQ, 0),
                        #    'output_control2': (XTS_ID_BASEBAND_AMPLITUDE_PHASE, 0),
